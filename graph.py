5d1db9ed72d5cf3e0adabaf440ea1ae43f640998
edwardwawrzynek <edward@wawrzynek.com>
2019-06-17 16:00:45 -0600

    add mouseover value trace

--- graph.py
+++ graph.py
@@ -268,12 +268,11 @@ class ComplexSelectionType:
     REAL = 0
     IMAGINARY = 1
     ANGLE_RAD = 2
-    ANGLE_DEG = 3
-    MAG = 4
+    MAG = 3
 
 #represents a source for a plot and options for it
 class PlotSource:
-    def __init__(self, channel, y_axes):
+    def __init__(self, channel, y_axes, update_callback):
         self.channel = channel
         self.color = Gdk.RGBA(np.random.random_sample(), np.random.random_sample(), np.random.random_sample())
         self.dimSelect = {}
@@ -281,6 +280,7 @@ class PlotSource:
 
         self.y_axis = None
         self.y_axes = y_axes
+        self.update_callback = update_callback
     
     def setYAxes(self, y_axes):
         self.y_axes = y_axes
@@ -288,9 +288,11 @@ class PlotSource:
     def axisSelected(self, widget):
         index = widget.get_active()
         self.y_axis = self.y_axes[index]
+        self.update_callback()
     
     def setColor(self, widget):
         self.color = widget.get_rgba()
+        self.update_callback()
 
     def addToGrid(self, grid, v_index, remove_callback):
         grid.attach(Gtk.Label(self.channel.parent.name + ": " + self.channel.name), 0, v_index, 1, 1)
@@ -324,11 +326,12 @@ class PlotSource:
         #now add attributes based on channel type
         h_index = 4
         if self.channel.isComplex():
+            box = Gtk.HBox()
+            box.pack_start(Gtk.Label("Complex Mode: "), False, False, 0)
             type_sel = Gtk.ComboBoxText()
             type_sel.append_text("Real Part")
             type_sel.append_text("Imaginary Part")
-            type_sel.append_text("Angle (radians)")
-            type_sel.append_text("Angle (degrees)")
+            type_sel.append_text("Angle")
             type_sel.append_text("Magnitude")
 
             if self.complexMode != -1:
@@ -336,16 +339,37 @@ class PlotSource:
 
             def setComplexType(widget):
                 self.complexMode = widget.get_active()
+                self.update_callback()
 
             type_sel.connect("changed", setComplexType)
+            box.pack_start(type_sel, False, False, 0)
+            grid.attach(box, h_index, v_index, 1, 1)
+            h_index+=1
+        for dim in self.channel.getAdditionalDims():
+            if not dim in self.dimSelect:
+                self.dimSelect[dim] = 0
 
-            grid.attach(type_sel, h_index, v_index, 1, 1)
-            v_index+=1
+            def makeSetDim(dim):
+                def set(res):
+                    self.dimSelect[dim] = res
+                    self.update_callback()
+                return set
+
+            dim_input = DevOptionGUI("%s: " % dim, "int", makeSetDim(dim), doTypeLabel=False, default=self.dimSelect[dim])
+            grid.attach(dim_input.getComponent(), h_index, v_index, 1, 1)
+            h_index+=1
     
-    def getData(self):
+
+
+    def getXRData(self):
+        if self.channel.getData() is None:
+            return None, None, None, None
         #read data and add units
-        xData = self.channel.getData()[self.channel.getXAxisDim()].data * self.channel.getData().units[self.channel.getXAxisDim()]
-        yData = self.channel.getData().data * self.channel.getData().units['']
+        xData = self.channel.getData()[self.channel.getXAxisDim()]
+        yData = self.channel.getData().sel(self.dimSelect)
+
+        yUnit = self.channel.getData().units['']
+        xUnit = self.channel.getData().units[self.channel.getXAxisDim()]
 
         if self.complexMode != ComplexSelectionType.NONE:
             if self.complexMode == ComplexSelectionType.REAL:
@@ -354,13 +378,17 @@ class PlotSource:
                 yData = yData.imag
             elif self.complexMode == ComplexSelectionType.ANGLE_RAD:
                 yData = xr.ufuncs.angle(yData)
-            elif self.complexMode == ComplexSelectionType.ANGLE_DEG:
-                #manual unit correction required
-                yData = xr.ufuncs.angle(yData, deg=True) * (u.deg / u.rad)
+                yUnit = u.rad
             elif self.complexMode == ComplexSelectionType.MAG:
                 yData = np.absolute(yData)
 
-        return xData, yData
+        return xData, yData, xUnit, yUnit
+    
+    def getData(self):
+        xd, yd, xu, yu = self.getXRData()
+        if xd is None:
+            return None, None
+        return xd.data*xu, yd.data*yu
             
 
     @staticmethod
@@ -440,7 +468,6 @@ class Plot(Output):
 
         super().__init__(name, remove_callback)
 
-
     def newAxis(self):
         axis = PlotAxis("axis name", u.V, None, None)
         self.y_axes.append(axis)
@@ -448,7 +475,7 @@ class Plot(Output):
         self.regenerateAxisMenu()
         self.regenerateOutput()
         self.updateSourceYAxes()
-    
+
     def removeAxis(self, index):
         self.y_axes.pop(index)
 
@@ -473,7 +500,10 @@ class Plot(Output):
         i = len(self.box.get_children())
 
         self.graph_vbox, self.canvas, self.subplots = makePlot(self.x_axis, self.y_axes, lambda: [self.regenerateOutput(), self.regenerateSourceMenu()])
+        self.canvas.mpl_connect("motion_notify_event", self.handleMouseover)
         self.box.pack_start(self.graph_vbox, False, False, 0)
+        self.value_trace = Gtk.Label("(Hover over plot to see trace of values)", halign=Gtk.Align.START)
+        self.box.pack_start(self.value_trace, False, False, 0)
         self.box.pack_start(Gtk.Label("Y-axes"), False, False, 6)
         self.box.pack_start(PlotAxis.makeAxesMenu(self.y_axes, self.removeAxis, self.newAxis), False, False, 0)
         self.box.pack_start(Gtk.Label("X-Axis"), False, False, 6)
@@ -488,40 +518,39 @@ class Plot(Output):
     def regenerateOutput(self):
         self.box.remove(self.box.get_children()[0])
         self.graph_vbox, self.canvas, self.subplots = makePlot(self.x_axis, self.y_axes, lambda: [self.regenerateOutput(), self.regenerateSourceMenu()])
+        self.canvas.mpl_connect("motion_notify_event", self.handleMouseover)
 
         self.box.pack_start(self.graph_vbox, False, False, 0)
         self.box.reorder_child(self.graph_vbox, 0)
-
+        self.graphInput()
         self.canvas.draw()
         self.box.show_all()
     
     def regenerateAxisMenu(self):
-        self.box.remove(self.box.get_children()[2])
+        self.box.remove(self.box.get_children()[3])
         menu = PlotAxis.makeAxesMenu(self.y_axes, self.removeAxis, self.newAxis)
         self.box.pack_start(menu, False, False, 0)
-        self.box.reorder_child(menu, 2)
-
+        self.box.reorder_child(menu, 3)
+        self.graphInput()
         self.box.show_all()
     
     def regenerateSourceMenu(self):
-        self.box.remove(self.box.get_children()[5])
+        self.box.remove(self.box.get_children()[6])
         menu = PlotSource.makeSourceMenu(self.plotSources, self.removeSource)
         self.box.pack_start(menu, False, False, 0)
-        self.box.reorder_child(menu, 5)
-
+        self.box.reorder_child(menu, 6)
+        self.graphInput()
         self.box.show_all()
 
     #hook when adding source
     def checkSource(self, source):
-        self.plotSources.append(PlotSource(source, self.y_axes))
+        self.plotSources.append(PlotSource(source, self.y_axes, self.graphInput))
         self.regenerateSourceMenu()
 
     def inputAvailable(self):
-        self.graphInputs()
-        self.canvas.draw()
-        self.box.show_all()
+        self.graphInput()
     
-    def graphInputs(self):
+    def graphInput(self):
         for plt in self.subplots:
             plt.lines = []
         for src in self.plotSources:
@@ -541,7 +570,40 @@ class Plot(Output):
                 yData = yData.to(src.y_axis.unit)
             except:
                 continue
+            
             subplot.plot(xData, yData, color=[src.color.red, src.color.green, src.color.blue])
+        
+        self.canvas.draw()
+        self.box.show_all()
+    
+    def handleMouseover(self, event):
+        #if we have previously drawn a line, remove it
+        try:
+            self.subplots[0].lines.remove(self.mouseover_line)
+        except:
+            pass
+
+        if event.inaxes is None:
+            return
+        x_pos = event.xdata
+        self.mouseover_line = self.subplots[0].axvline(x=x_pos, lw=1, ls='dashed', color='k')
+        text = ""
+        for src in self.plotSources:
+            xData, yData, xUnit, yUnit = src.getXRData()
+            if xData is None:
+                continue
+            y_pos = yData.interp({src.channel.getXAxisDim(): x_pos}).data * yUnit
+            try:
+                y_pos = y_pos.to(src.y_axis.unit)
+            except:
+                continue
+        
+            text += src.channel.parent.name + ": " + src.channel.name + ": " + str(y_pos) + "\n"
+        
+        self.value_trace.set_text(text)
+
+        self.canvas.draw()
+        self.box.show_all()
 '''
 #A display for data output
 class Plot(Output):
